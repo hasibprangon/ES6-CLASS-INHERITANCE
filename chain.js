be08ab4fb34b6feac39f0bdb26f8cc6e18e149df
hasibprangon <hasibulhossainp09@gmail.com>
2023-04-14 11:08:15 -0700

    chain

---
+++ chain.js
@@ -0,0 +1,27 @@
+const users = [{id:1, name:'hasib', job:'student'}];
+
+// console.log(users[0].name);
+
+const data = {
+    count: 500,
+    data: [
+        {id: 1, name:'hasib', job:'student'} ,
+        {id: 2, name:'prangon', job:'developer'} 
+    ]
+}
+const first_job = data.data[0].job;
+// console.log(first_job);
+
+const human = {
+    id : 1938320105,
+    name:'Md. Hasibul Hossain Prangon',
+    address:{
+        street:{
+            first: 'Bus stand road',
+            second:'Bujruk Mirjapur',
+            third:'Rice mill'
+        },
+        postOffice:'janipur',
+        city:'Kushtia'
+    }
+}
