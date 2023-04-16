9b8999ba51610738a5ada5bbf79f7cac57ecfcb9
hasibprangon <hasibulhossainp09@gmail.com>
2023-04-16 07:47:24 -0700

    class.js

---
+++ class.js
@@ -0,0 +1,23 @@
+class Instructor {
+name;
+designation = 'web course instructor'
+item = 'web team'
+location;
+constructor(name,location){
+    this.name = name;
+    this.location = location;
+}
+startSupportSession (time){
+    console.log(`Start support session at ${time}`)
+}
+creatQuiz(module){
+    console.log(`Creat quiz for module ${module}`)
+}
+}
+
+const hasib = new Instructor('Hasib', 'Kushtia');
+console.log(hasib);
+hasib.startSupportSession('9.00')
+hasib.creatQuiz(32);
+const prangon = new Instructor('Prangon', 'Pabna');
+console.log(prangon);
