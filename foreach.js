8b9690c7cc67e81315aa8c7af3f3eea1e4c3a21d
hasibprangon <hasibulhossainp09@gmail.com>
2023-04-16 01:12:26 -0700

    foreach

---
+++ foreach.js
@@ -0,0 +1,11 @@
+const friends = ['Hasibul Hossain', 'Asad', 'Sabbir', 'Imran', 'Babul'];
+
+friends.forEach(f => console.log(f));
+
+const product = [
+    {id:1, name:'laptop', price:25000,},
+    {id:2, name:'mobile', price:23000,},
+    {id:3, name:'watch', price:15000,},
+    {id:4, name:'bike', price:250000,},
+];
+
